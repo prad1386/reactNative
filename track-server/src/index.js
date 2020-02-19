7cd1b9d534575088a0ce17430f33289b194c2f3f
Pradeep Kumar <pradeepkumar@Pradeeps-MacBook-Air.local>
2020-02-19 19:18:17 +0530

    track path on map react native app

---
+++ track-server/src/index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const authRoutes = require("./routes/authRoutes");
+
+const app = express();
+app.use(bodyParser.json());
+app.use(authRoutes);
+
+const mongoUri =
+  "mongodb+srv://admin:passwordpassword@cluster0-h2ji1.mongodb.net/test?retryWrites=true&w=majority";
+mongoose.connect(mongoUri, {
+  useNewUrlParser: true,
+  useCreateIndex: true
+});
+
+mongoose.connection.on("connected", () => {
+  console.log("--- Connected to mongo instance ----");
+});
+
+mongoose.connection.on("error", err => {
+  console.log("Error connection to mongo", err);
+});
+
+app.get("/", (req, res) => {
+  res.send("Hi There");
+});
+
+app.listen(3000, () => {
+  console.log("Listening on port 3000");
+});
