a8136a075918a02806353035dd9b85d2a074b818
Pradeep Kumar <pradeepkumar@Pradeeps-MacBook-Air.local>
2020-02-23 21:20:03 +0530

    tracks model and routes

--- track-server/src/index.js
+++ track-server/src/index.js
@@ -1,13 +1,16 @@
 require("./models/User");
+require("./models/Track");
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const authRoutes = require("./routes/authRoutes");
+const trackRoutes = require("./routes/trackRoutes");
 const requireAuth = require("./middleware/requireAuth");
 
 const app = express();
 app.use(bodyParser.json());
 app.use(authRoutes);
+app.use(trackRoutes);
 
 const mongoUri =
   "mongodb+srv://admin:passwordpassword@cluster0-h2ji1.mongodb.net/test?retryWrites=true&w=majority";
